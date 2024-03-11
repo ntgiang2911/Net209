850a3afe883bfcfa5eef2673c99c5749e01d2859
ntgia <ntgia@DESKTOP-OT4C32N>
2024-03-11 21:04:29 +0700

    Add project files.

---
+++ Net209/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net209
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap vao so nguyen i = ");
+            int i = int.Parse(Console.ReadLine());
+
+            //int i = 10;
+            /*if ( i%2 == 0) 
+            {
+                Console.WriteLine("Day la so chan ");
+            }
+            else
+            {
+                Console.WriteLine("Day la so le ");
+            };*/
+
+            //dung toan tu 3 ngoi
+
+            Console.WriteLine(i % 3 == 0 ? "So chia het cho 3" : "So khong chia het cho 3" ); // ? = if, : = else
+            
+            Console.ReadKey();
+        }
+    }
+}
